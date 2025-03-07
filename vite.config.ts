10da3ce0877c57c56b3200eff68ef0cabc1eaf13
Caio Mizumoto <caio.mizumoto@cloudwalk.io>
2025-03-06 21:04:47 -0300

    fixing more issued with git hub pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Change this to use relative paths
+  base: '/frontend-test-starwars/', // Match your repository name
 })
